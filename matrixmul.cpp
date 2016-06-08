41050df418acad917dedc4759dcc0081a2b9854c
Krzysztof Leszczynski <leszczynski.christopher@gmail.com>
2016-06-08 16:00:03 +0200

    AColumn v 0.1

--- matrixmul.cpp
+++ matrixmul.cpp
@@ -75,32 +75,37 @@ int main(int argc, char * argv[])
 		return 3;
 	}
 
-	comm_start =  MPI_Wtime();
-
 	int c = repl_fact;
 	int p = num_processes;
 	int s = p / c;
-	int q = (s + c - 1) / c;
+	int q;
+	if(use_inner) q = (s + c - 1) / c;
+	else q = p / c;
 
-	vector<char*> messages(num_processes);
-	MPI_Request requests[num_processes * 2];
-	MPI_Status statuses[num_processes * 2];
+	char* messages;
 
 	InitMessage my_init_msg;
 	PartA A;
 	PartB B;
 	PartA C;
-	InitMessage init_msgs[s][c];
+	InitMessage *init_msgs = (InitMessage*)malloc(p * sizeof(InitMessage));
+	vector<vector<Point>> inputA;
+
+	int bytes;
+	char *sendbuf, *recvbuf;
+	int *sendcount, *displs, *recvcount;
+
+	int m, n, nnz, max_nnz;
+
 	//read
 	if(mpi_rank == 0) {
-		int m, n, nnz, max_nnz;
 		FILE *file = fopen(sparse_filename, "r");
 		fscanf(file, "%d%d%d%d", &m, &n, &nnz, &max_nnz);
-	
+
 		vector<double> line1(nnz);
 		vector<int> line2(m + 1);
 		vector<int> line3(nnz);
-		vector<vector<Point>> inputA(m);
+		inputA.resize(m);
 
 		for(int i = 0; i < nnz; i++) fscanf(file, "%lf", &line1[i]);
 		for(int i = 0; i < m + 1; i++) fscanf(file, "%d", &line2[i]);
@@ -111,12 +116,18 @@ int main(int argc, char * argv[])
 				inputA[i].push_back(Point(line3[idx], line1[idx]));
 			}
 		}
-		
+	}
+
+	comm_start =  MPI_Wtime();
+
+	vector<PartA> partA(s);
+	int *ids = (int*)malloc(c * sizeof(int));
+	memset(ids, 0, c * sizeof(int));
+
+	if(mpi_rank == 0) {	
 		// set A columns and rows
 		if(use_inner) {
 			int length = (n + s - 1) / s;
-			vector<PartA> partA(s);
-			int ids[c];
 
 			ids[0] = 0;
 			for(int i = 1; i < c; i++) {
@@ -127,16 +138,17 @@ int main(int argc, char * argv[])
 				for(int j = 0; j < c; j++) {
 					int idx = (ids[j] + i) % s;
 					int idy = j;
+					int idp = idx * c + idy;
 
-					init_msgs[idx][idy].firstBColumn = length * idx;
-					init_msgs[idx][idy].lastBColumn = min(length * (idx + 1), n);
+					init_msgs[idp].firstBColumn = length * idx;
+					init_msgs[idp].lastBColumn = min(length * (idx + 1), n);
 
-					init_msgs[idx][idy].cx = idx;
-					init_msgs[idx][idy].cy = idy;
+					init_msgs[idp].cx = idx;
+					init_msgs[idp].cy = idy;
 
-					init_msgs[idx][idy].n = m;
-					init_msgs[idx][idy].nnz = nnz;
-					init_msgs[idx][idy].max_nnz = max_nnz;
+					init_msgs[idp].n = m;
+					init_msgs[idp].nnz = nnz;
+					init_msgs[idp].max_nnz = max_nnz;
 				}
 				partA[i].first = row;
 				partA[i].last = min(row + length, n);
@@ -145,87 +157,109 @@ int main(int argc, char * argv[])
 					partA[i].vecs[row_idx - row] = inputA[row_idx];
 				}
 			}
+		}
+		else {
+			int stepA = (n + c - 1) / c, stepB = (n + p - 1) / p;
+			for(int i = 0; i < p; i++) {
+				init_msgs[i].n = n;
+				init_msgs[i].nnz = nnz;
+				init_msgs[i].max_nnz = max_nnz;
+				init_msgs[i].num_rounds = s;
+				init_msgs[i].cx = i / c;
+				init_msgs[i].cy = i % c;
 
-			for(int i = 1; i < p; i++) {
-				int idx = (i/c) % s;
-				int idy = i % c;
-				MPI_Isend(
-					&init_msgs[idx][idy],
-					sizeof(InitMessage),
-					MPI_BYTE,
-					i,
-					MPI_INIT_TAG,
-					MPI_COMM_WORLD,
-					&requests[2 * i]
-				);
+				init_msgs[i].firstBColumn = min(i * stepB, n);
+				init_msgs[i].lastBColumn = min((i + 1) * stepB, n);
 
-				int partAIdx = (idx + ids[idy]) % s;
-				int bytes = analysePartA(partA[partAIdx]);
-				messages[i] = (char*)malloc(bytes);
-				partAToMessage(partA[partAIdx], messages[i]);
-				MPI_Isend(
-					messages[i],
-					bytes,
-					MPI_BYTE,
-					i,
-					MPI_INIT_A_TAG,
-					MPI_COMM_WORLD,
-					&requests[2 * i + 1]
-				);
+				init_msgs[i].firstARow = 0;
+				init_msgs[i].lastARow = n;
+
+				init_msgs[i].firstAColumn = min((i / c) * stepA, n);
+				init_msgs[i].lastAColumn = min(n,((i / c) + 1) * stepA);
 			}
-			my_init_msg = init_msgs[0][0];
-			A = partA[0];
+
+			for(int i = 0; i < s; i++) {
+				partA[i].first = 0;
+				partA[i].last = n;
+				partA[i].vecs.resize(n);
+			}
+
+			for(int i = 0, step = (n + s - 1) / s; i < n; i++) {
+				int j = 0;
+				for(auto &p: inputA[i]) {
+					while(p.idx >= (j + 1) * step) j++;
+					partA[j].vecs[i].push_back(p);
+				}
+			}
+		}
+		my_init_msg = init_msgs[0];
+		A = partA[0];
+	}
+
+	MPI_Scatter(init_msgs, sizeof(InitMessage), MPI_BYTE,
+				&my_init_msg, sizeof(InitMessage), MPI_BYTE,
+				0, MPI_COMM_WORLD);
+
+	if(mpi_rank == DEBUG_RANK) cerr << my_init_msg << "\n";
+
+	MPI_Comm group_comm;
+	MPI_Comm_split(MPI_COMM_WORLD, my_init_msg.cx, my_init_msg.cy, &group_comm);
+	if(mpi_rank == DEBUG_RANK) {
+		int t;
+		MPI_Comm_size(group_comm, &t);
+		cerr << "Comm size: " << t << "\n";
+	} 
+
+	n = my_init_msg.n;
+	nnz = my_init_msg.nnz;
+	bytes = nnz * sizeof(Point) + s * n * sizeof(int) + s * 2 * sizeof(int);
+	if(mpi_rank == 0) {
+		sendbuf = (char*)malloc(c * bytes);
+		sendcount = (int*)malloc(p * sizeof(int));
+		displs = (int*)malloc((p + 1) * sizeof(int));
+	}
+	recvbuf = (char*)malloc(bytes);
+
+	if(mpi_rank == 0) {
+		displs[0] = 0;
+		for(int i = 0; i < p; i++) {
+			int idx = (i/c) % s;
+			int idy = i % c;
+
+			int partAIdx = (idx + ids[idy]) % s;
+			int bytes = analysePartA(partA[partAIdx]);
+
+			messages = sendbuf + displs[i];
+			partAToMessage(partA[partAIdx], messages);
+			sendcount[i] = bytes;
+			displs[i + 1] = displs[i] + bytes;
 		}
 	}
+
+	if(mpi_rank == DEBUG_RANK) cerr << "I'm about to scatter\n";
+
+	MPI_Scatterv(sendbuf, sendcount, displs, MPI_BYTE, recvbuf, bytes,
+		MPI_BYTE, 0, MPI_COMM_WORLD);
+
+	if(mpi_rank == DEBUG_RANK) cerr << "I'm about to convert message to A\n";
+	if(mpi_rank > 0) A = messageToPartA(recvbuf);
+	if(mpi_rank == DEBUG_RANK) {
+		cerr << "I'm " << mpi_rank << "(" << my_init_msg.cx << ", " << my_init_msg.cy  << "), and got:\n";
+		cerr << A; 
+	}
+
+	if(mpi_rank == 0) {
+		free(sendbuf);
+		free(sendcount);
+		free(displs);
+	}
+	free(recvbuf);
+	free(ids);
 	
 	MPI_Request recv_request, send_request;
 	MPI_Status recv_status, send_status;
 
-	// RECEIVE MATRIX A
-	if(mpi_rank > 0) {
-		MPI_Irecv(
-			&my_init_msg, 
-			sizeof(InitMessage),
-			MPI_BYTE,
-			0,
-			MPI_INIT_TAG,
-			MPI_COMM_WORLD,
-			&recv_request
-		);
-
-		MPI_Wait(&recv_request, &recv_status);
-	}
-
-	int rows_per_proc = (my_init_msg.n + s - 1) / s;
-	int max_bytes = rows_per_proc * my_init_msg.max_nnz * sizeof(Point) + (2 + rows_per_proc) * sizeof(int);
-	char *recv_msg = (char*)malloc(max_bytes);
-	char *send_msg = (char*)malloc(max_bytes);
-
-	if(mpi_rank > 0) {
-		MPI_Irecv(
-			recv_msg,
-			max_bytes,
-			MPI_BYTE,
-			0,
-			MPI_INIT_A_TAG,
-			MPI_COMM_WORLD,
-			&recv_request
-		);
-
-		MPI_Wait(&recv_request, &recv_status);
-		A = messageToPartA(recv_msg);
-		if(mpi_rank == DEBUG_RANK) {
-			cerr << "I'm " << mpi_rank << "(" << my_init_msg.cx << ", " << my_init_msg.cy  << "), and got:\n";
-			cerr << A; 
-		}
-	}
-	if(mpi_rank == 0) {
-		MPI_Waitall((num_processes - 1) * 2, requests + 2, statuses + 2);
-		for(int i = 1; i < p; i++) free(messages[i]);
-	}
-
 	// GENERATE MATRIX B
-	int n = my_init_msg.n;
 	B = PartB(my_init_msg.firstBColumn, my_init_msg.lastBColumn);
 	B.columns.resize(my_init_msg.lastBColumn - my_init_msg.firstBColumn, vector<double>(n));
 	for(int i = 0, col = my_init_msg.firstBColumn; col < my_init_msg.lastBColumn; col++, i++) {
@@ -239,7 +273,15 @@ int main(int argc, char * argv[])
 
 	// CREATE MATRIX C
 	C = PartA(B.first, B.last);
-	C.vecs.resize(C.last - C.first);
+	if(use_inner) C.vecs.resize(C.last - C.first);
+	else {
+		C.vecs.resize(C.last - C.first, vector<Point>(n, Point(0, 0)));
+		FOR(i, col, C) {
+			for(int j = 0; j < n; j++) C.vecs[i][j].idx = j;
+		}
+	}
+
+	if(mpi_rank == DEBUG_RANK) cerr << C;
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	comm_end = MPI_Wtime();
@@ -247,25 +289,25 @@ int main(int argc, char * argv[])
 	// ***************************************
 	// ************* COMPUTATION *************
 	// ***************************************
-	int bytes = ((n + s - 1) / s) * ((n + s - 1) / s) * q * sizeof(Point) + (2 + ((n + s - 1) / s)) * sizeof(int);
-	char *sendbuf, *recvbuf;
-	int *recvcount, *displs;
-	sendbuf = (char*)malloc(bytes);
-	recvbuf = (char*)malloc(bytes * c);
-	recvcount = (int*)malloc(c * sizeof(int));
-	displs = (int*)malloc(c * sizeof(int));
-	for(int i = 0; i < c; i++) {
-		recvcount[i] = bytes;
-		displs[i] = i * bytes;
+	if(mpi_rank == DEBUG_RANK) cerr << "Start computation\n";
+	
+	if(use_inner) {
+		bytes = ((n + s - 1) / s) * ((n + s - 1) / s) * q * sizeof(Point) + (2 + ((n + s - 1) / s)) * sizeof(int);
+
+		sendbuf = (char*)malloc(bytes);
+		recvbuf = (char*)malloc(bytes * c);
+		recvcount = (int*)malloc(c * sizeof(int));
+		displs = (int*)malloc(c * sizeof(int));
+		for(int i = 0; i < c; i++) {
+			recvcount[i] = bytes;
+			displs[i] = i * bytes;
+		}
 	}
 
-	MPI_Comm group_comm;
-	MPI_Comm_split(MPI_COMM_WORLD, my_init_msg.cx, my_init_msg.cy, &group_comm);
-	if(mpi_rank == DEBUG_RANK) {
-		int t;
-		MPI_Comm_size(group_comm, &t);
-		cerr << "Comm size: " << t << "\n";
-	} 
+	char *send_msg, *recv_msg;
+	int max_bytes = nnz * sizeof(Point) + n * sizeof(int) + 2 * sizeof(int);
+	send_msg = (char*)malloc(max_bytes);
+	recv_msg = (char*)malloc(max_bytes);
 
 	comp_start = MPI_Wtime();
 
@@ -275,28 +317,31 @@ int main(int argc, char * argv[])
 			int idx = my_init_msg.cx;
 			int idy = my_init_msg.cy;
 
+			int next_rank, prev_rank;
+
+			if(use_inner) {
+				if(t) prev_rank = c * ((idx + s - 1) % s) + idy;
+				else prev_rank = c * ((idx + q - 1) % s) + idy;
+
+				if(t) next_rank = c * ((idx + 1) % s) + idy;
+				else next_rank = c *((idx - q + 1 + s) % s) + idy;
+			}
+			else {
+				prev_rank = (mpi_rank - c + p) % p;
+				next_rank = (mpi_rank + c) % p;
+			}
+
 			// TODO - don't send and receive last message
 			int bytes = partAToMessage(A, send_msg);
-			if(t)
-				MPI_Isend(
-					send_msg,
-					bytes,
-					MPI_BYTE,
-					c * ((idx + s - 1) % s) + idy,
-					MPI_SEND_A,
-					MPI_COMM_WORLD,
-					&send_request
-				);
-			else
-				MPI_Isend(
-					send_msg,
-					bytes,
-					MPI_BYTE,
-					c * ((idx + q - 1) % s) + idy,
-					MPI_SEND_A,
-					MPI_COMM_WORLD,
-					&send_request
-				);
+			MPI_Isend(
+				send_msg,
+				bytes,
+				MPI_BYTE,
+				prev_rank,
+				MPI_SEND_A,
+				MPI_COMM_WORLD,
+				&send_request
+			);
 
 			if(mpi_rank == DEBUG_RANK)
 				cerr << A;
@@ -307,30 +352,20 @@ int main(int argc, char * argv[])
 					for(auto it = A.vecs[i].begin(); it != A.vecs[i].end(); it++) {
 						el += it->x * B.columns[j][it->idx];
 					}
-					C.vecs[j].push_back(Point(row, el));
+					if(use_inner) C.vecs[j].push_back(Point(row, el));
+					else C.vecs[j][row].x += el;
 				}
 			}
 
-			if(t)
-				MPI_Irecv(
-					recv_msg,
-					max_bytes,
-					MPI_BYTE,
-					c * ((idx + 1) % s) + idy,
-					MPI_SEND_A,
-					MPI_COMM_WORLD,
-					&recv_request
-				);
-			else
-				MPI_Irecv(
-					recv_msg,
-					max_bytes,
-					MPI_BYTE,
-					c *((idx - q + 1 + s) % s) + idy,
-					MPI_SEND_A,
-					MPI_COMM_WORLD,
-					&recv_request
-				);
+			MPI_Irecv(
+				recv_msg,
+				max_bytes,
+				MPI_BYTE,
+				next_rank,
+				MPI_SEND_A,
+				MPI_COMM_WORLD,
+				&recv_request
+			);
 
 			
 			MPI_Wait(&send_request, &send_status);
@@ -341,56 +376,52 @@ int main(int argc, char * argv[])
 		}
 
 		if(!exponent) break;
+		if(use_inner) {
+			// Broadcast C
+			int sendcount = partAToMessage(C, sendbuf);
+			MPI_Allgatherv(sendbuf, sendcount, MPI_BYTE, recvbuf, recvcount, displs,
+				MPI_BYTE, group_comm);
 
-		// Broadcast C
-		int sendcount = partAToMessage(C, sendbuf);
-		MPI_Allgatherv(sendbuf, sendcount, MPI_BYTE, recvbuf, recvcount, displs,
-			MPI_BYTE, group_comm);
-
-		for(int i = 0; i < c; i++) {
-			PartA tempC = messageToPartA(recvbuf + displs[i]);
-			FOR(j, row, tempC) {
-				for(auto &p: tempC.vecs[j]) {
-					if(mpi_rank == DEBUG_RANK) cerr << p << "\n";
-					B.columns[j][p.idx] = p.x;
+			for(int i = 0; i < c; i++) {
+				PartA tempC = messageToPartA(recvbuf + displs[i]);
+				FOR(j, row, tempC) {
+					for(auto &p: tempC.vecs[j]) {
+						if(mpi_rank == DEBUG_RANK) cerr << p << "\n";
+						B.columns[j][p.idx] = p.x;
+					}
 				}
 			}
+
+			if(mpi_rank == DEBUG_RANK) cerr << B;
+
+			C = PartA(C.first, C.last);
+			C.vecs.resize(C.last - C.first);
 		}
-
-		if(mpi_rank == DEBUG_RANK) cerr << B;
-
-		C = PartA(C.first, C.last);
-		C.vecs.resize(C.last - C.first);
+		else {
+			FOR(i, col, C)
+				for(auto &p: C.vecs[i])
+					B.columns[i][p.idx] = p.x;
+			C = PartA(C.first, C.last);
+			C.vecs.resize(C.last - C.first, vector<Point>(n, Point(0,0)));
+			FOR(i, col, C) {
+				for(int j = 0; j < n; j++)
+					C.vecs[i][j].idx = j;
+			}
+		}
 	}
 
 	if(mpi_rank == DEBUG_RANK)
 				cerr << A;
 
-	// Sort partC
-	/*for(auto &col : C.vecs) {
-		if(col.size() == 1) continue;
-		auto ptr = col.begin();
-		for(; next(ptr) != col.end(); ptr++)
-			if(ptr->idx > next(ptr)->idx) break;
-		ptr++;
-
-		if(mpi_rank == DEBUG_RANK) {
-			if(ptr == col.end())
-				cerr << "Point end\n";
-			else
-				cerr << *ptr << "\n";
-		}
-
-		col.splice(col.begin(), col, ptr, col.end());
-	}*/
-
 	MPI_Barrier(MPI_COMM_WORLD);
 	comp_end = MPI_Wtime();
 
-	free(sendbuf);
-	free(recvbuf);
-	free(recvcount);
-	free(displs);
+	if(use_inner) {
+		free(sendbuf);
+		free(recvbuf);
+		free(recvcount);
+		free(displs);
+	}
 	MPI_Comm_free(&group_comm);
 
 	free(recv_msg);
@@ -402,6 +433,7 @@ int main(int argc, char * argv[])
 	
 	if (show_results) 
 	{
+		int rows_per_proc = (my_init_msg.n + s - 1) / s;
 		double bigC[n][n];
 		memset(bigC, 0, n * n * sizeof(double));
 		int bytes = rows_per_proc * n * sizeof(Point) + (2 + rows_per_proc) * sizeof(int);
@@ -424,7 +456,7 @@ int main(int argc, char * argv[])
 			sendbuf = (char*)malloc(bytes);
 			sendcount = partAToMessage(C, sendbuf);
 		}
-
+		if(mpi_rank == DEBUG_RANK) cerr << "I'm going to gather\n";
 		MPI_Gatherv(sendbuf, sendcount, MPI_BYTE, recvbuf, recvcount,
 			displs, MPI_BYTE, 0, MPI_COMM_WORLD);
 
@@ -464,6 +496,20 @@ int main(int argc, char * argv[])
 	return 0;
 }
 
+/*void collectA(const InitMessage &msg, PartA &A, int p) {
+	char *sendbuf, *recvbuf;
+	int *recvcount, *displs;
+	int sendcount;
+
+	sendcount = analysePartA(A);
+	sendbuf = (char*)malloc(sendcount);
+
+	free(sendbuf);
+	free(recvbuf);
+	free(recvcount);
+	free(displs);
+}*/
+
 int analysePartA(const PartA &partA) {
 	int size = sizeof(int) * (2 + partA.last - partA.first);
 	int count = 0;
@@ -498,15 +544,6 @@ int partAToMessage(const PartA &partA, char *out) {
 
 	return size;
 }
-
-/*int partCToMessage(const PartC &c, char *out) {
-	PartA a(c.firstColumn, c.lastColumn);
-	a.vecs = vector<vector<Point>>(c.columns.size());
-	for(unsigned int i = 0; i < c.columns.size(); i++)
-		a.vecs[i] = vector<Point> { c.columns[i].begin(), c.columns[i].end() };
-
-	return partAToMessage(a, out);
-}*/
 
 PartA messageToPartA(char *out) {
 	PartA partA;
@@ -549,6 +586,7 @@ ostream& operator<<(ostream& os, const PartA &a) {
 ostream& operator<<(ostream& os, const PartB &b) {
 	os << "==== PartB ====\n";
 	os << b.first << " " << b.last << "\n";
+	if(b.last == b.first) return os;
 	for(unsigned int j = 0; j < b.columns[0].size(); j++) {
 		for(int i = 0, col = b.first; col < b.last; col++, i++) {
 			os << b.columns[i][j] << " ";
@@ -559,22 +597,10 @@ ostream& operator<<(ostream& os, const PartB &b) {
 	return os;
 }
 
-/*ostream& operator<<(ostream& os, const PartC &c) {
-	os << "==== PartC ====\n";
-	os << c.firstColumn << " " << c.lastColumn << "\n";
-	vector< decltype(c.columns[0].begin()) > ptr;
-	for(int i = 0, col = c.firstColumn; col < c.lastColumn; col++, i++)
-		ptr.push_back(c.columns[i].begin());
-	for(int j = 0; j < c.n; j++) {
-		for(int i = 0, col = c.firstColumn; col < c.lastColumn; col++, i++) {
-			if(ptr[i]->idx == j) {
-				os << ptr[i]->x << " ";
-				ptr[i]++;
-			}
-			else os << 0 << " ";
-		}
-		os << "\n";
-	}
+ostream& operator<<(ostream& os, const InitMessage &a) {
+	os << a.n << " " << a.nnz << " " << a.max_nnz << "\n";
+	os << a.firstBColumn << " " << a.lastBColumn << "\n";
+	os << a.firstARow << " " << a.lastARow << "\n";
 
 	return os;
-}*/
+}
